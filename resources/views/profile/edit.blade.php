edd6715c45650bfb985af18d480eed9c7dca642e
agnestiw <agnestiwulansari605@gmail.com>
2025-04-28 08:02:47 +0700

    profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -18,7 +18,7 @@
 
                     <form action="{{ route('profile.update') }}" method="POST">
                         @csrf
-                        @method('PUT')
+                        @method('patch')
                         <div class="mb-4">
                             <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Name:</label>
                             <input type="text" name="name" id="name" value="{{ $user->name }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
